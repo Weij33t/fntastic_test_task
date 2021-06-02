310fceff66d5227c2ace69994cb12c1b17a1887e
Weij33t <43062352+Weij33t@users.noreply.github.com>
2021-06-02 20:47:09 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+const servers = [
+  {
+    name: 'Weij33t`s server',
+    rooms: [
+      {
+        name: 'First room',
+        channels: [
+          {
+            name: 'First text channel',
+            messages: [
+              {
+                content:
+                  'firsfirst message t messagfirst message e first messafirst message ge ',
+                author: {
+                  name: 'wiej33t',
+                  color: 'purple',
+                },
+                time: '01/06/2021',
+              },
+            ],
+            users: [
+              {
+                name: 'weij33t',
+                color: 'red',
+                role: 'not bot',
+                id: '#2021',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
